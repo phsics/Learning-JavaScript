3c7f449676f17a9283d450e76388a5ab5f11a355
Rohan Kumar <112564780+phsics@users.noreply.github.com>
2024-10-22 17:54:15 +0000

    datatypes

---
+++ 01basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+// JavaScript is a dynamically typed language, which means that data types of 
+// variables are determined by the value they hold at runtime and can change 
+// throughout the program as we assign different values to them.
+
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
