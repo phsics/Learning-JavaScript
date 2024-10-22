9b731d34c5581e5de21315a0d9506a51360a4f87
Rohan Kumar <112564780+phsics@users.noreply.github.com>
2024-10-22 16:48:55 +0000

    changes

---
+++ 01basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "hitesh";
+
+// console.log(type of score);
+// console.log(type of(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "rohan";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
