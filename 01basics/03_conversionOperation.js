f79381f5ff67ca83a01fd0905ddaf16421fbae63
Rohan Kumar <112564780+phsics@users.noreply.github.com>
2024-10-22 17:26:18 +0000

    conversion

--- 01basics/03_conversionOperation.js
+++ 01basics/03_conversionOperation.js
@@ -24,3 +24,18 @@ let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+//  ******************* Operations *******************
+
+let str1 = "hello";
+let str2 = "hitesh";
+
+let str3 =str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
