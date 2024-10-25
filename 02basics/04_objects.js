ef450e5aa305fc53464c5b915f0134999d15cbd3
Rohan Kumar <112564780+phsics@users.noreply.github.com>
2024-10-25 15:39:40 +0000

    04

--- 02basics/04_objects.js
+++ 02basics/04_objects.js
@@ -33,6 +33,31 @@ const obj2 = {3: "a",4: "b"};
 
 // Correct way to combine two objects
 
-const obj3 = Object.assign({},obj1, obj2);
-console.log(obj3);
+// const obj3 = Object.assign({},obj1, obj2);
+// console.log(obj3);
 
+const obj3 = {...obj1, ...obj2};
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "1@gmail.com"
+    },
+    {
+        id: 2,
+        email: "2@gmail.com"
+    },
+    {
+        id: 3, 
+        email: "3@gmail.com"
+    }
+]
+// console.log(users[0].id);
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
