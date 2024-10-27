bb4a3695d47f1dcc227f9ce7802cdc02dec89b95
Rohan Kumar <112564780+phsics@users.noreply.github.com>
2024-10-27 15:29:24 +0000

    control flow

---
+++ 04control_flow/01_control_flow.js
@@ -0,0 +1,39 @@
+// if
+
+// const isUserloggedIn = true;
+
+// if(isUserloggedIn){
+
+// }
+
+// if(2 === "2"){
+//     console.log("executed");
+// }
+// <, >, <=, >=, ==, !=, ===
+
+// const temperature = 40 
+
+// if(temperature === 40){
+//     console.log("less than 50");
+// }
+// else {
+//     console.log("temperature is greater than 50")
+// }
+
+// const score = 200 
+
+// if(score > 100){
+//     const power = "fly";
+//     console.log(`User power: ${power}`);
+// }
+
+const isUserloggedIn = true;
+const debitCard = true;
+
+if(isUserloggedIn && debitCard){
+    console.log("Allow to buy course");
+}
+else{
+    console.log("Not allowed to buy course");
+}
+
